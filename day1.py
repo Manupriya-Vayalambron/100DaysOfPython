aba984aa937d407529a273b9ae5362bb652a7fe2
Manupriya-Vayalambron <manupriyadhanushvayalambron@gmail.com>
2025-09-30 16:55:56 +0530

    Initial commit

---
+++ day1.py
@@ -0,0 +1,4 @@
+print("Welcome to the Brand name Generator.")
+city=input("Whats's the name of the city you grew up in?\n")
+pet=input("What's your pet's name?\n")
+print("Your brand name could be "+city+" "+pet);
