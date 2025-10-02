758cb8ac8fccdd4e889d99d617cdd9ecae9ffe9a
Manupriya Dhanush Vayalambron <125171620+Manupriya-Vayalambron@users.noreply.github.com>
2025-10-02 12:48:31 +0530

    Band Name Generator
    Updated comment for clarity on band name formation.

--- day1.py
+++ day1.py
@@ -9,6 +9,6 @@ city=input("Whats's the name of the city you grew up in?\n")
 # Step 3: Ask the user for the name of a pet
 pet=input("What's your pet's name?\n")
 
-# Step 4: Combine the city and pet name to form the band name
+# Step 4: Combine the city and pet name to form band name (demo for concatenation)
 print("Your brand name could be "+city+" "+pet);
 
